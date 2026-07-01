3260ff33ae8b955aa355f8a032ef3efd16f8f58e
Trishanth Sai <trishanth.yuvaraj@gmail.com>
2026-07-02 01:35:39 +0530

    Implemented RWMutex  and proper https status codes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"time"
+	"sync"
 )
 
 type cacheEntry struct {
@@ -13,28 +14,42 @@ type cacheEntry struct {
 }
 
 var cache = map[string]cacheEntry{}
+var mu sync.RWMutex
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	key := r.URL.Path
+	mu.RLock()
 	entry, exists := cache[key]
+	mu.RUnlock()
 	if exists && time.Now().Before(entry.expiresAt) {
-		fmt.Println(w, "Cache hit")
+		fmt.Printf("%s %s Cache hit\n", r.Method, r.URL.Path)
 		w.Write(entry.body)
 		return
 	} else {
-		fmt.Println(w, "Cache miss")
-		response, err := http.Get("http://dummyjson.com/products")
+		fmt.Printf("%s %s Cache miss\n", r.Method, r.URL.Path)
+		baseURL := "http://dummyjson.com"
+		fullURL := baseURL + key
+		response, err := http.Get(fullURL)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
+		defer response.Body.Close()
 		body, err := io.ReadAll(response.Body)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
+		if(response.StatusCode==200){
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			mu.Lock()
+			cache[key] = cacheEntry{body: body, expiresAt: time.Now().Add(5 * time.Minute)}
+			mu.Unlock()
+			w.WriteHeader(response.StatusCode)
+			w.Write(body)
+		}else{
+			w.WriteHeader(response.StatusCode)
+	    	w.Write(body)
 		}
-		cache[key] = cacheEntry{body: body, expiresAt: time.Now().Add(5 * time.Minute)}
-		w.Write(body)
 	}
 }
 func main() {
